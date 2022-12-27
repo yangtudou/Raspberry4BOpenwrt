91e11fdf221fb7d1cd086d1dbb411f90b7dcee3b
yang_potato <yangtudou1220@icloud.com>
2022-12-27 12:06:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,10 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 修改默认 ip 地址 因为本人主要是做旁路由实用所以改为我自己的 ip 段内的，方便开箱即用。
+sed -i 's/192.168.1.1/192.168.1.10/g' package/base-files/files/bin/config_generate
+
+# 修改主机名
+#pushd package/base-files/files/bin
+#sed -i 's/ImmortalWrt/OpenWrt/g' config_generate
+#popd
